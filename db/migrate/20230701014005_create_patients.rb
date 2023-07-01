32dedbe58c36f57ae3f4b514c0641f2554b28452
Kender <kenderromain@gmail.com>
2023-06-30 21:41:21 -0400

    Generates controller, model, migration, view, and test files for patient.

---
+++ db/migrate/20230701014005_create_patients.rb
@@ -0,0 +1,15 @@
+class CreatePatients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :date_of_birth
+      t.string :date
+      t.string :address
+      t.string :phone
+      t.string :reference
+
+      t.timestamps
+    end
+  end
+end
