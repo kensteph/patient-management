32e4d2d6b92fd63843fd38678f54e374e8c5b0bf
Kender <kenderromain@gmail.com>
2023-07-02 12:55:00 -0400

    Generates controller, model, migration, and view for service.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  resources :services
   resources :appointments
   resources :patients
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "appointments#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_01_014005) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_01_142108) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.date "app_date"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.bigint "patient_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
+    t.index ["user_id"], name: "index_appointments_on_user_id"
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string "first_name"
@@ -31,4 +42,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_01_014005) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "appointments", "patients"
+  add_foreign_key "appointments", "users"
 end
